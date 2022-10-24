e051168adf93791eb5a780874f5c80e89278bde3
Jue Jue Paing <juejuepaing@Jues-MacBook-Pro.local>
2022-10-24 16:04:27 +0630

    test

--- src/controllers/hourly.js
+++ src/controllers/hourly.js
@@ -2,6 +2,11 @@ const models = require('../database/models')
 
 exports.getHourlyUsage = async (req, res) => {
 
+  return res.status(200).json({
+    status: 'success',
+    test: 'good'
+  });
+
   const result = await models.Hourly.findAll({
     include: [{
       model: models.Daily,
