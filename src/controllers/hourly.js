7c6342fa834d172a32527a1e0ca4e2413e291bd1
Jue Jue Paing <juejuepaing@Jues-MacBook-Pro.local>
2022-10-24 17:29:03 +0630

    test

--- src/controllers/hourly.js
+++ src/controllers/hourly.js
@@ -2,21 +2,30 @@ const models = require('../database/models')
 
 exports.getHourlyUsage = async (req, res) => {
 
-  const result = await models.Hourly.findAll({
-    include: [{
-      model: models.Daily,
-        as: 'daily'
-    }]
-   })
-
-  if (!result.length)
-    return res.status(404).json({
+  try {
+    const result = await models.Hourly.findAll({
+      include: [{
+        model: models.Daily,
+          as: 'daily'
+      }]
+     })
+  
+    if (!result.length)
+      return res.status(404).json({
+        status: 'success',
+        message: 'No Result Found',
+      });
+  
+    return res.status(200).json({
       status: 'success',
-      message: 'No Result Found',
+      result,
     });
-
-  return res.status(200).json({
-    status: 'success',
-    result,
-  });
+  }
+  catch (err) {
+      
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Something wrong!!!'
+    });
+  }
 };
