5777cd4b52d6af5c934d7660367b1ee678083d32
Jue Jue Paing <juejuepaing@Jues-MacBook-Pro.local>
2022-10-24 17:51:55 +0630

    f

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -14,12 +14,12 @@ module.exports = {
         dialect: 'mysql',
     },
     production: {
-        use_env_variable: process.env.DATABASE_URL
+        use_env_variable: process.env.DATABASE_URL,
         // username: process.env.PROD_DB_USERNAME,
         // password: process.env.PROD_DB_PASSWORD,
         // database: process.env.PROD_DB_DATABASE,
         // host: process.env.PROD_DB_HOST,
-        // dialect: 'mysql',
+        dialect: 'mysql',
     },
 };
 
